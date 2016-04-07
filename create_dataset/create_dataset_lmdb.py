b83f764fd3d7afce66cfc218a139b0ccf51c6dec
Johannes <johannes.laurmaa@epfl.ch>
2016-04-07 17:03:06 +0900

    Remove hard coded variables in create_dataset_lmdb.py.

--- create_dataset/create_dataset_lmdb.py
+++ create_dataset/create_dataset_lmdb.py
@@ -1,5 +1,5 @@
 # Create the dataset with images in the file_with_paths
-# python create_dataset_lmdb.py
+# python create_dataset_lmdb.py --W 480 --H 360 --output_path /home/pierre/camvid/val_gt_lmdb/ --input_images /home/shared/datasets/CamVid/val_lab.txt --nb_classes 13 --labels True
 
 import caffe
 import lmdb
@@ -7,24 +7,34 @@ from PIL import Image
 import numpy as np
 import glob
 from random import shuffle
+import argparse
 
-
-
-H = 360 # Required Height
-W = 480 # Required Width
-dataset_name = '/home/pierre/camvid/val_gt_lmdb/' # Folder where to create the dataset
-file_with_paths = '/home/shared/datasets/CamVid/val_lab.txt' # Text file with all the paths
-labels = True # Set to true if ground truth
-number_of_classes = 12 # Number of classes in the dataset
-
-
-
+def get_arguments():
+    # Import arguments
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--W', type=int, required=True, \
+                                    help=   'Resize image to this width')
+    parser.add_argument('--H', type=int, required=True, \
+                                    help=   'Resize image to this height')
+    parser.add_argument('--output_path', type=str, required=True, \
+                                    help=   'Path to output LMDB folder')
+    parser.add_argument('--input_images', type=str, required=True, \
+                                    help=   'Path to .txt file with all images to store in the LMDB database')
+    parser.add_argument('--nb_classes', type=str, default=255, \
+                                    help=   'Number of classes. Only required for labels')
+    parser.add_argument('--labels', type=bool, default=False, \
+                                    help=   'Precise if dealing with labels and not RGB')
+    return parser.parse_args()
 
 def main(args):
     
-    in_db = lmdb.open(dataset_name, map_size=int(1e12))
+    # Get all options
+    args = get_arguments()
+    
+    in_db = lmdb.open(args.output_path, map_size=int(1e12))
     with in_db.begin(write=True) as in_txn:
-        for in_idx, in_ in enumerate(open(file_with_paths)):
+        for in_idx, in_ in enumerate(open(args.input_images)):
             print 'img: ' + str(in_idx)
             
             # load image:
@@ -32,10 +42,10 @@ def main(args):
             # save type
             Dtype = im.dtype
             
-            if labels:
+            if args.labels:
                 # Resize the input image
                 Limg = Image.fromarray(im)
-                Limg = Limg.resize([W, H],Image.NEAREST)
+                Limg = Limg.resize([args.W, args.H],Image.NEAREST)
                 im = np.array(Limg,Dtype)
                 # Convert from HxWxC (C=3) to HxWxC (C=1)
                 im = im.reshape(im.shape[0],im.shape[1],1)
@@ -43,13 +53,13 @@ def main(args):
                 # RGB to BGR
                 im = im[:,:,::-1]
                 im = Image.fromarray(im)
-                im = im.resize([W, H], Image.ANTIALIAS)
+                im = im.resize([args.W, args.H], Image.ANTIALIAS)
                 im = np.array(im,Dtype)
             
             # Convert to CxHxW
             im = im.transpose((2,0,1))
-            if labels:
-                im[im==255]=number_of_classes
+            if args.labels:
+                im[im==255]=args.nb_classes
             
             # Create the dataset
             im_dat = caffe.io.array_to_datum(im)
