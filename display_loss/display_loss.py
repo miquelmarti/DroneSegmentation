9b4525443c1e39ee357e6589c2b3249974d4083f
Andrew Holliday <ahollid@cim.mcgill.ca>
2016-04-12 14:44:31 +0900

    Updated display_loss.py so that it can be applied directly to caffe logs.
    This makes get_losses.sh obsolete, so it has been removed.  Also display_loss.py now computes the iteration message frequency on its own from the messages in the logs, so this does not need to be entered by the user.

--- display_loss/display_loss.py
+++ display_loss/display_loss.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Takes, as inputs, the file with all the losses and the frequency of the display (display parameter in the solver.prototxt used for the training). Plot the loss then, with matplotlib.
 # Use : python display_loss.py loss.txt 20
 
@@ -5,20 +7,29 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import operator
+
+
+TARGET_STRS = ['Iteration', 'loss']
+ITER_VAL_IDX = 5
+LOSS_VAL_IDX = 9
 
 
 parser = argparse.ArgumentParser()
-
 # Mandatory options
-parser.add_argument('text_file_with_losses', type=str, help='Path to the file that lists all the loss')
-parser.add_argument('display_frequency', type=int, help='Number between each displayed iteration')
+parser.add_argument('log_file', type=str, help='Path to the Caffe output log file')
+# parser.add_argument('display_frequency', type=int, help='Number between each displayed iteration')
 
 args = parser.parse_args()
 
-
-loss = [float(line.rstrip('\n')) for line in open(args.text_file_with_losses)]
-x = np.arange(0, len(loss)*args.display_frequency, args.display_frequency)
-
-plt.plot(x, loss)
-
-plt.show()
+with open(args.log_file, 'r') as f:
+    
+    lossLines = [l for l in f if reduce(lambda x,y: x and y,
+                                        [t in l for t in TARGET_STRS])]
+    getIterFn = lambda l: int(l.split(' ')[ITER_VAL_IDX].strip(','))
+    iterNums = [getIterFn(l) for l in lossLines]
+    frequency = getIterFn(lossLines[1]) - getIterFn(lossLines[0])
+    losses = [float(l.strip().rpartition(' ')[2]) for l in lossLines]
+    x = np.arange(0, len(losses)*frequency, frequency)
+    plt.plot(x, losses)
+    plt.show()
