95e74bf52402a377f7650e5cebfe4c071032119b
Pierre Ecarlat <pierre.ecarlat@gmail.com>
2016-04-14 19:53:24 +0900

    update deploy and train val for camvid, camvid_lmdb and pascal (all segnet)

--- caffeSegNet/train_segnet-camvid/train.sh
+++ caffeSegNet/train_segnet-camvid/train.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export PYTHONPATH="/home/shared/caffeSegNet/python"
-/home/shared/caffeSegNet/build/tools/caffe train --solver="/home/pierre/hgRepos/models/caffeSegNet/train_segnet-camvid/solver.prototxt" 2>&1 | tee /home/pierre/hgRepos/models/caffeSegNet/train_segnet-camvid/train/info.log
+/home/shared/caffeSegNet/build/tools/caffe train --solver="/home/pierre/hgRepos/models/caffeSegNet/train_segnet-camvid/solver.prototxt" --snapshot="/home/pierre/hgRepos/models/caffeSegNet/train_segnet-camvid/train/train_iter_10000.solverstate" 2>&1 | tee /home/pierre/hgRepos/models/caffeSegNet/train_segnet-camvid/train/info2.log
--- caffeSegNet/train_segnet-pascal/train.sh
+++ caffeSegNet/train_segnet-pascal/train.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export PYTHONPATH="/home/shared/caffeSegNet/python"
-/home/shared/caffeSegNet/build/tools/caffe train --solver="/home/pierre/hgRepos/models/caffeSegNet/train_segnet-pascal/solver.prototxt" --gpu 0 2>&1 | tee /home/pierre/hgRepos/models/caffeSegNet/train_segnet-pascal/train/info.log
+/home/shared/caffeSegNet/build/tools/caffe train --solver="/home/pierre/hgRepos/models/caffeSegNet/train_segnet-pascal/solver.prototxt" 2>&1 | tee /home/pierre/hgRepos/models/caffeSegNet/train_segnet-pascal/train/info.log
