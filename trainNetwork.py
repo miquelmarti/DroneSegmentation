f6453e67833a9ac938d2c3803469636161c8a685
Andrew Holliday <ahollid@cim.mcgill.ca>
2016-05-19 19:31:14 +0900

    - Improved some argument names - Fixed some incorrect import statements - Corrected a bug - we need to actually change directory to the location of the solver.

--- trainNetwork.py
+++ trainNetwork.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 
 import argparse
-import solve
+import os
+from caffeUtils import solve, fcnSurgery
 
+SOLVER_FILENAME = 'solver.prototxt'
 
 def getArguments():
     '''
     If running solve.py from command line, read in command-line arguments.
     '''
     parser = argparse.ArgumentParser()
-    parser.add_argument('--modelFilename', type=str, help='\
+    parser.add_argument('--weights', type=str, help='\
     Weights to use to initialize the network.')
     parser.add_argument('--device', type=int, default=0, help='\
     ID of the GPU device to train.')
@@ -21,9 +23,8 @@ def getArguments():
     If the metric score on the test_net differs by less than this percentage \
     value between tests, halt.  If not provided, continue for iterations \
     specified by solver file's max_iter.")
-
-    parser.add_argument('solver',
-                        help='Filename of the solver prototxt file.')
+    parser.add_argument('learn_dir', type=str,
+                        help='Directory containing training .prototxt files')
     args = parser.parse_args()
     return args
 
@@ -32,7 +33,8 @@ if __name__ == "__main__":
     args = getArguments()
     preProcFun = None
     if args.fcn:
-        import fcnSurgery
         preProcFun = fcnSurgery.fcnInterp
-    solve.solve(args.solver, args.modelFilename, preProcFun, args.device,
+        
+    os.chdir(args.learn_dir)
+    solve.solve(SOLVER_FILENAME, args.weights, preProcFun, args.device,
                 args.halt_percent, silent=False)
