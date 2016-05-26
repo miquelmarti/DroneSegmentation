6fd98877a42c962155ff426dfaf9f74b1f27cb61
Pierre Ecarlat <pierre.ecarlat@gmail.com>
2016-05-26 16:44:46 +0900

    Test ; modify job3.sh temporarly

--- multiSource/job3.sh
+++ multiSource/job3.sh
@@ -2,3 +2,5 @@
 
 LOG_FILENAME=logs/job3_$(date "+%F-%T").log
 ~/hgRepos/transferLearningFramework/scripts/transfer.py --gpu $1 3_fcnresnet_coco_pascal.prototxt --clean 2>&1 | tee $LOG_FILENAME
+
+echo 'yeay !'
