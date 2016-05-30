a77941756a1bb583e20868c474188c5225bde1a7
Andrew Holliday <ahollid@cim.mcgill.ca>
2016-05-30 17:38:12 +0900

    - Corrected the handling of files with data from multi-source jobs. - Added ability to display the scores of the best weights computed during a multi-source job.

--- display_loss.py
+++ display_loss.py
@@ -32,6 +32,8 @@ SEG_TEST_PREFIX = '>>>'
 SEG_TEST_AVOID = '.cpp'
 MS_ITER_START_STR = 'starting multi-source iteration'
 STAGE_START_STR = '-> Execute stage'
+OUT_MODEL_STR = '-> Produce the model'
+BEST_SCORE_STR = 'New best score for stage'
 
 
 def getIterNum(line):
@@ -77,27 +79,36 @@ def plotXAndY(x, y, title):
 
 
 def plotLearningLogs(lossLines, segTestLines, displayTagDict, prefix=''):
+    # sort the lines by their date stamps
+    if displayTagDict[LOSS_TAG]:
+        plotLosses(lossLines, prefix)
+    plotSegTests(segTestLines, displayTagDict, prefix)
+
+
+def plotLosses(lossLines, prefix=''):
     iterNums = map(getIterNum, lossLines)
     losses = [float(l.strip().rpartition(' ')[2]) for l in lossLines]
-    if displayTagDict[LOSS_TAG]:
-        plotXAndY(iterNums, losses, prefix + 'training loss')
+    plotXAndY(iterNums, losses, prefix + 'training loss')
 
+
+def plotSegTests(segTestLines, displayTagDict, prefix=''):
     testStats = getTestStats(segTestLines)
     for tag in testStats:
-        if displayTagDict[tag]:
+        if displayTagDict[tag] and len(testStats[tag]) > 0:
             iterations, stats = zip(*testStats[tag])
             plotXAndY(iterations, stats, prefix + tag)
-    
+            
     
 parser = argparse.ArgumentParser(description="\
 Plots statistics produced by the fcn.berkeleyvision.org solving process.  \
 Arguments may be provided to select which statistics to display.  If no \
 arguments are provided, all statistics will be shown.")
-parser.add_argument('-1', '--merge', action="store_true",
-                    help='Plot stats from all multi-source stages on a \
-                    single graph.')
+# parser.add_argument('-1', '--merge', action="store_true",
+#                     help='Plot stats from all multi-source stages on a \
+#                     single graph.')
 parser.add_argument('log_file', type=str,
                     help='Path to the Caffe output log file')
+parser.add_argument('--bestonly', action='store_true')
 disp_group = parser.add_argument_group('Graphs to display')
 disp_group.add_argument('--' + TRN_LOSS_ARG, action="store_true",
                         help='Display the training loss.')
@@ -126,35 +137,53 @@ else:
 with open(args.log_file, 'r') as f:
     lossLines = []
     segTestLines = []
+    bestScoreLines = []
     msIter = None
     stage = ''
+    prefix = ''
     currentIter = 0
-
+    
     for l in f:
-        if MS_ITER_START_STR in l:
+        if l.startswith(OUT_MODEL_STR):
+            # seg-test messages for the job are done, so plot them
+            plotSegTests(segTestLines, displayTagDict, prefix)
+            segTestLines = []
+
+        # these cases are simply for labelling of plots
+        elif l.startswith(MS_ITER_START_STR):
             postStr = l.partition(MS_ITER_START_STR)[2]
             msIter = int(postStr.split()[0])
-        if STAGE_START_STR in l:
+        elif l.startswith(STAGE_START_STR):
             stage = l.partition(STAGE_START_STR)[2].strip()
-        if not (l.startswith(SEG_TEST_PREFIX) or SEG_TEST_AVOID in l):
+            prefix = ' '.join(['MS Iter', str(msIter), ',', stage, ':'])
+        # remaining cases are aggregating data
+        elif l.startswith(BEST_SCORE_STR):
+            bestScoreLines.append(l)
+        elif not (l.startswith(SEG_TEST_PREFIX) or SEG_TEST_AVOID in l):
             continue  # ignore badly-formatted lines
         elif l.startswith(SEG_TEST_PREFIX) and SEG_TEST_AVOID not in l:
             segTestLines.append(l)
-        elif all([t in l for t in TRAIN_LOSS_TAGS]):
+        elif all([t in l for t in TRAIN_LOSS_TAGS]) and not args.bestonly:
+            # if iteration goes down, we must be in a new job
             newIter = getIterNum(l)
-            if newIter < currentIter:  # and not args.merge:
-                # when values return to zero, we finished a "stage", so plot
-                prefix = ' '.join(['MS Iter', str(msIter), ',', stage, ':'])
-                plotLearningLogs(lossLines, segTestLines, displayTagDict,
-                                 prefix)
+            if newIter < currentIter and displayTagDict[LOSS_TAG]:
+                plotLosses(lossLines, prefix)
                 lossLines = []
-                segTestLines = []
             lossLines.append(l)
             currentIter = newIter
 
     # display the remaining collected values
-    if msIter is not None:
-        prefix = ' '.join(['MS Iter', str(msIter), ',', stage, ':'])
-        plotLearningLogs(lossLines, segTestLines, displayTagDict, prefix)
-    else:
-        plotLearningLogs(lossLines, segTestLines, displayTagDict)
+    if not args.bestonly:
+        if msIter is not None:  # and not args.merge:
+            plotLearningLogs(lossLines, segTestLines, displayTagDict, prefix)
+        else:
+            plotLearningLogs(lossLines, segTestLines, displayTagDict)
+
+    if len(bestScoreLines) > 0:
+        bestScores = [float(l.partition(':')[2]) for l in bestScoreLines]
+        firstStage = bestScoreLines[0].partition(':')[0].split()[-1]
+        plt.plot(range(len(bestScores)), bestScores, 'bs')
+        plt.title('starting at ' + firstStage)
+        plt.show()
+
+        
