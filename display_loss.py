ca21f4a42226c84e57ec72d282333b6e1928bb5c
Andrew Holliday <ahollid@cim.mcgill.ca>
2016-05-30 15:12:28 +0900

    Improved handling of multi-source stages in display_loss.py.

--- display_loss.py
+++ display_loss.py
@@ -77,8 +77,6 @@ def plotXAndY(x, y, title):
 
 
 def plotLearningLogs(lossLines, segTestLines, displayTagDict, prefix=''):
-    if msIter is not None:
-        prefix = 'MS iteratation ' + str(msIter) + ':'
     iterNums = map(getIterNum, lossLines)
     losses = [float(l.strip().rpartition(' ')[2]) for l in lossLines]
     if displayTagDict[LOSS_TAG]:
@@ -95,6 +93,9 @@ parser = argparse.ArgumentParser(description="\
 Plots statistics produced by the fcn.berkeleyvision.org solving process.  \
 Arguments may be provided to select which statistics to display.  If no \
 arguments are provided, all statistics will be shown.")
+parser.add_argument('-1', '--merge', action="store_true",
+                    help='Plot stats from all multi-source stages on a \
+                    single graph.')
 parser.add_argument('log_file', type=str,
                     help='Path to the Caffe output log file')
 disp_group = parser.add_argument_group('Graphs to display')
@@ -126,26 +127,34 @@ with open(args.log_file, 'r') as f:
     lossLines = []
     segTestLines = []
     msIter = None
-    prefix = ''
+    stage = ''
+    currentIter = 0
 
     for l in f:
         if MS_ITER_START_STR in l:
             postStr = l.partition(MS_ITER_START_STR)[2]
-            prefix = 'MS iter. ' + postStr.split()[0]
+            msIter = int(postStr.split()[0])
         if STAGE_START_STR in l:
-            prefix += l.partition(STAGE_START_STR)[2]
-            # display the values computed up to this point
-            if len(lossLines) > 0 and len(segTestLines) > 0:
-                plotLearningLogs(lossLines, segTestLines, displayTagDict,
-                                 prefix)
-                lossLines = []
-                segTestLines = []
+            stage = l.partition(STAGE_START_STR)[2].strip()
         if not (l.startswith(SEG_TEST_PREFIX) or SEG_TEST_AVOID in l):
             continue  # ignore badly-formatted lines
         elif l.startswith(SEG_TEST_PREFIX) and SEG_TEST_AVOID not in l:
             segTestLines.append(l)
         elif all([t in l for t in TRAIN_LOSS_TAGS]):
+            newIter = getIterNum(l)
+            if newIter < currentIter:  # and not args.merge:
+                # when values return to zero, we finished a "stage", so plot
+                prefix = ' '.join(['MS Iter', str(msIter), ',', stage, ':'])
+                plotLearningLogs(lossLines, segTestLines, displayTagDict,
+                                 prefix)
+                lossLines = []
+                segTestLines = []
             lossLines.append(l)
+            currentIter = newIter
 
     # display the remaining collected values
-    plotLearningLogs(lossLines, segTestLines, displayTagDict, prefix)
+    if msIter is not None:
+        prefix = ' '.join(['MS Iter', str(msIter), ',', stage, ':'])
+        plotLearningLogs(lossLines, segTestLines, displayTagDict, prefix)
+    else:
+        plotLearningLogs(lossLines, segTestLines, displayTagDict)
