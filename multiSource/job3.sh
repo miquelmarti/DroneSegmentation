e0028d3ccbe5148e12433e598ebf441736ed9435
Andrew Holliday <ahollid@cim.mcgill.ca>
2016-06-01 19:08:58 +0900

    Merged upstream changes.

--- multiSource/job3.sh
+++ multiSource/job3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 LOG_FILENAME=logs/job3_$(date "+%F-%T").log
-~/hgRepos/transferLearningFramework/scripts/transfer.py --gpu $1 3_fcnresnet_coco_pascal.prototxt 2>&1 | tee $LOG_FILENAME
+~/transferLearningFramework/scripts/transfer.py --gpu $1 3_fcnresnet_coco_pascal.prototxt 2>&1 | tee $LOG_FILENAME
--- multiSource/job4.sh
+++ multiSource/job4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 LOG_FILENAME=logs/job4_$(date "+%F-%T").log
-~/hgRepos/transferLearningFramework/scripts/transfer.py --gpu $1 4_fcnresnet_cocopascal.prototxt 2>&1 | tee $LOG_FILENAME
+~/transferLearningFramework/scripts/transfer.py --gpu $1 4_fcnresnet_cocopascal.prototxt 2>&1 | tee $LOG_FILENAME
