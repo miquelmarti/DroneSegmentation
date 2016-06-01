6cf3011889a71a1c8ac783fda1777353cd941826
Andrew Holliday <ahollid@cim.mcgill.ca>
2016-06-01 17:17:39 +0900

    - Added a function to display a several smoothed loss curves. - Compressed test statistics plots into just two plots, one for test loss and one for the rest. - Removed now-extraneous arguments and logic.

--- display_loss.py
+++ display_loss.py
@@ -5,35 +5,32 @@
 
 
 import argparse
-import matplotlib.pyplot as plt
 import string
+import os
+import matplotlib.pyplot as plt
+import numpy as np
 
 ITER_TAG = "Iteration"
-LOSS_TAG = "loss"
+TRN_LOSS_TAG = "loss = "
+TST_LOSS_TAG = "loss"
 OV_ACC_TAG = "overall accuracy"
 MEAN_ACC_TAG = "mean accuracy"
 MEAN_IU_TAG = "mean IU"
 FWAVACC_TAG = "fwavacc"
+TRAIN_LOSS_TAGS = [ITER_TAG, TRN_LOSS_TAG]
 
-TRN_LOSS_ARG = "train_loss"
-TST_LOSS_ARG = "val_loss"
-OV_ACC_ARG = "ov_acc"
-MEAN_ACC_ARG = "mean_acc"
-MEAN_IU_ARG = "mean_iu"
-FWAVACC_ARG = "fwavacc"
-
-TAGS_TO_ARGS = {LOSS_TAG: TST_LOSS_ARG,
-                OV_ACC_TAG: OV_ACC_ARG,
-                MEAN_ACC_TAG: MEAN_ACC_ARG,
-                MEAN_IU_TAG: MEAN_IU_ARG,
-                FWAVACC_TAG: FWAVACC_ARG}
-TRAIN_LOSS_TAGS = [ITER_TAG, LOSS_TAG]
 SEG_TEST_PREFIX = '>>>'
 SEG_TEST_AVOID = '.cpp'
 MS_ITER_START_STR = 'starting multi-source iteration'
 STAGE_START_STR = '-> Execute stage'
 OUT_MODEL_STR = '-> Produce the model'
 BEST_SCORE_STR = 'New best score for stage'
+
+
+def isTrainLossLine(line):
+    return (not line.startswith(SEG_TEST_PREFIX) and
+            SEG_TEST_AVOID in line and
+            all([tag in line for tag in TRAIN_LOSS_TAGS]))
 
 
 def getIterNum(line):
@@ -52,7 +49,7 @@ def getTaggedValue(line, tag, valType=float):
 
 
 def getTestStats(lines):
-    tags = [LOSS_TAG,
+    tags = [TST_LOSS_TAG,
             OV_ACC_TAG,
             MEAN_ACC_TAG,
             MEAN_IU_TAG,
@@ -78,112 +75,124 @@ def plotXAndY(x, y, title):
     plt.show()
 
 
-def plotLearningLogs(lossLines, segTestLines, displayTagDict, prefix=''):
+def plotMultiple(xs, ys, labels, title):
+    for x, y, label in zip(xs, ys, labels):
+        plt.plot(x, y, label=label)
+    plt.title(title)
+    plt.legend(bbox_to_anchor=(0.7, 1), loc=2, borderaxespad=0.)
+    plt.show()
+    
+
+def plotLearningLogs(lossLines, segTestLines, prefix=''):
     # sort the lines by their date stamps
-    if displayTagDict[LOSS_TAG]:
-        plotLosses(lossLines, prefix)
-    plotSegTests(segTestLines, displayTagDict, prefix)
+    plotTrainingLosses(lossLines, prefix)
+    plotSegTests(segTestLines, prefix)
 
 
-def plotLosses(lossLines, prefix=''):
+def plotTrainingLosses(lossLines, prefix=''):
     iterNums = map(getIterNum, lossLines)
     losses = [float(l.strip().rpartition(' ')[2]) for l in lossLines]
     plotXAndY(iterNums, losses, prefix + 'training loss')
 
 
-def plotSegTests(segTestLines, displayTagDict, prefix=''):
+def plotSegTests(segTestLines, prefix=''):
+    if len(segTestLines) is 0:
+        return
     testStats = getTestStats(segTestLines)
+    # plot loss separately, since it exists on a very different scale
+    testLossIters, testLosses = zip(*testStats[TST_LOSS_TAG])
+    plotXAndY(testLossIters, testLosses, prefix + 'test loss')
+    del(testStats[TST_LOSS_TAG])
+
+    # plot other statistics
+    testIters, testVals = [], []
     for tag in testStats:
-        if displayTagDict[tag] and len(testStats[tag]) > 0:
-            iterations, stats = zip(*testStats[tag])
-            plotXAndY(iterations, stats, prefix + tag)
+        statIters, statVals = zip(*testStats[tag])
+        testIters.append(statIters)
+        testVals.append(statVals)
+    plotMultiple(testIters, testVals, testStats.keys(), prefix + 'stats')
+
+
+def plotSmoothedLossCurves(logList, avgOverLast=10, interval=1,
+                           title='Loss curves'):
+    allSmoothIters, allSmoothLosses, allNames = [], [], []
+    for logFile in logList:
+        with open(logFile, 'r') as f:
+            # extract the loss values and their iterations
+            points = [(getIterNum(l), getTaggedValue(l, TRN_LOSS_TAG))
+                      for l in f if isTrainLossLine(l)]
+            iters, losses = zip(*points)
             
-    
-parser = argparse.ArgumentParser(description="\
-Plots statistics produced by the fcn.berkeleyvision.org solving process.  \
-Arguments may be provided to select which statistics to display.  If no \
-arguments are provided, all statistics will be shown.")
-# parser.add_argument('-1', '--merge', action="store_true",
-#                     help='Plot stats from all multi-source stages on a \
-#                     single graph.')
-parser.add_argument('log_file', type=str,
-                    help='Path to the Caffe output log file')
-parser.add_argument('--bestonly', action='store_true')
-disp_group = parser.add_argument_group('Graphs to display')
-disp_group.add_argument('--' + TRN_LOSS_ARG, action="store_true",
-                        help='Display the training loss.')
-disp_group.add_argument('--' + TST_LOSS_ARG, action="store_true",
-                        help='Display the validation loss.')
-disp_group.add_argument('--' + OV_ACC_ARG, action="store_true",
-                        help='Display the overall validation accuracy.')
-disp_group.add_argument('--' + MEAN_ACC_ARG, action="store_true",
-                        help='Display the mean validation accuracy.')
-disp_group.add_argument('--' + MEAN_IU_ARG, action="store_true",
-                        help='Display the validation mean IU.')
-disp_group.add_argument('--' + FWAVACC_ARG, action="store_true",
-                        help='Display the fwavacc (?) on the validation set.')
-args = parser.parse_args()
+            # Compute the average loss over the last avgOverLast losses
+            allSmoothIters.append(iters[avgOverLast-1::interval])
+            sampleIdxs = range(avgOverLast, len(points) + 1, interval)
+            smoothLoss = [np.mean(losses[i-avgOverLast:i]) for i in sampleIdxs]
+            allSmoothLosses.append(smoothLoss)
+            allNames.append(os.path.basename(logFile))
+            
+    # produce smoothedPlotSequences from lossSequences
+    plotMultiple(allSmoothIters, allSmoothLosses, allNames, title)
 
-# if no display arguments given, display all the plots
-argVars = vars(args)
-displayAll = not any([argVars[arg] for _, arg in TAGS_TO_ARGS.iteritems()])
-displayTagDict = None
-if displayAll:
-    displayTagDict = {tag: True for tag, arg in TAGS_TO_ARGS.iteritems()}
-else:
-    displayTagDict = {tag: argVars[arg]
-                      for tag, arg in TAGS_TO_ARGS.iteritems()}
 
-with open(args.log_file, 'r') as f:
-    lossLines = []
-    segTestLines = []
-    bestScoreLines = []
-    msIter = None
-    stage = ''
-    prefix = ''
-    currentIter = 0
-    
-    for l in f:
-        if l.startswith(OUT_MODEL_STR):
-            # seg-test messages for the job are done, so plot them
-            plotSegTests(segTestLines, displayTagDict, prefix)
-            segTestLines = []
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="\
+    Plots statistics produced by the fcn.berkeleyvision.org solving process.  \
+    Arguments may be provided to select which statistics to display.  If no \
+    arguments are provided, all statistics will be shown.")
+    # parser.add_argument('-1', '--merge', action="store_true",
+    #                     help='Plot stats from all multi-source stages on a \
+    #                     single graph.')
+    parser.add_argument('log_file', type=str,
+                        help='Path to the Caffe output log file')
+    args = parser.parse_args()
 
-        # these cases are simply for labelling of plots
-        elif l.startswith(MS_ITER_START_STR):
-            postStr = l.partition(MS_ITER_START_STR)[2]
-            msIter = int(postStr.split()[0])
-        elif l.startswith(STAGE_START_STR):
-            stage = l.partition(STAGE_START_STR)[2].strip()
-            prefix = ' '.join(['MS Iter', str(msIter), ',', stage, ':'])
-        # remaining cases are aggregating data
-        elif l.startswith(BEST_SCORE_STR):
-            bestScoreLines.append(l)
-        elif not (l.startswith(SEG_TEST_PREFIX) or SEG_TEST_AVOID in l):
-            continue  # ignore badly-formatted lines
-        elif l.startswith(SEG_TEST_PREFIX) and SEG_TEST_AVOID not in l:
-            segTestLines.append(l)
-        elif all([t in l for t in TRAIN_LOSS_TAGS]) and not args.bestonly:
-            # if iteration goes down, we must be in a new job
-            newIter = getIterNum(l)
-            if newIter < currentIter and displayTagDict[LOSS_TAG]:
-                plotLosses(lossLines, prefix)
-                lossLines = []
-            lossLines.append(l)
-            currentIter = newIter
+    with open(args.log_file, 'r') as f:
+        lossLines = []
+        segTestLines = []
+        bestScoreLines = []
+        msIter = None
+        stage = ''
+        prefix = ''
+        currentIter = 0
 
-    # display the remaining collected values
-    if not args.bestonly:
-        if msIter is not None:  # and not args.merge:
-            plotLearningLogs(lossLines, segTestLines, displayTagDict, prefix)
-        else:
-            plotLearningLogs(lossLines, segTestLines, displayTagDict)
+        for l in f:
+            if l.startswith(OUT_MODEL_STR):
+                # seg-test messages for the job are done, so plot them
+                plotSegTests(segTestLines, prefix)
+                segTestLines = []
 
-    if len(bestScoreLines) > 0:
-        bestScores = [float(l.partition(':')[2]) for l in bestScoreLines]
-        firstStage = bestScoreLines[0].partition(':')[0].split()[-1]
-        plt.plot(range(len(bestScores)), bestScores, 'bs')
-        plt.title('starting at ' + firstStage)
-        plt.show()
+            # these cases are simply for labelling of plots
+            elif l.startswith(MS_ITER_START_STR):
+                postStr = l.partition(MS_ITER_START_STR)[2]
+                msIter = int(postStr.split()[0])
+            elif l.startswith(STAGE_START_STR):
+                stage = l.partition(STAGE_START_STR)[2].strip()
+                prefix = 'MS Iter. ' + str(msIter) + ', ' + stage + ': '
+            # remaining cases are aggregating data
+            elif l.startswith(BEST_SCORE_STR):
+                bestScoreLines.append(l)
+            elif not (l.startswith(SEG_TEST_PREFIX) or SEG_TEST_AVOID in l):
+                continue  # ignore badly-formatted lines
+            elif l.startswith(SEG_TEST_PREFIX) and SEG_TEST_AVOID not in l:
+                segTestLines.append(l)
+            elif all([t in l for t in TRAIN_LOSS_TAGS]):
+                # if iteration goes down, we must be in a new job
+                newIter = getIterNum(l)
+                if newIter < currentIter:
+                    plotTrainingLosses(lossLines, prefix)
+                    lossLines = []
+                lossLines.append(l)
+                currentIter = newIter
 
-        
+        # display the remaining collected values
+        if msIter is None:
+            prefix = ''
+        plotTrainingLosses(lossLines, prefix)
+        plotSegTests(segTestLines, prefix)
+
+        if len(bestScoreLines) > 0:
+            bestScores = [float(l.partition(':')[2]) for l in bestScoreLines]
+            firstStage = bestScoreLines[0].partition(':')[0].split()[-1]
+            plt.plot(range(len(bestScores)), bestScores, 'bs')
+            plt.title('starting at ' + firstStage)
+            plt.show()
