792c464339516a0078fbc30e20e3802870066adb
Andrew Holliday <ahollid@cim.mcgill.ca>
2016-06-03 12:51:44 +0900

    Updated view_weights.py to print the shift and scale factors used for each layer it displays, along with the name of the layer, as comma-separated values.  Later we'd like to be able to feed this information back in from a .csv file and have the script apply the values contained therein to the relevant layers on a different set of weights.  Other small changes described below.
    - Updated the example usage comment to reflect current command-line arguments.
    - Improved the names of some command-line arguments.
    - Removed build_network method, moving its code into the "main" code.

--- view_weights.py
+++ view_weights.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 
 # Code for visualizing the main look of the weights
-# Ex : python view_weights.py --model /home/johannes/project/models/segnet/deploy-pascal.prototxt --weights /home/johannes/project/models/segnet/train_pascal/ --layer "conv1_1" --scale_divide 0.1 --scale_shift -0.05
+# Ex : python view_weights.py --model /home/johannes/project/models/segnet/deploy-pascal.prototxt --weights /home/johannes/project/models/segnet/train_pascal/ --layers "conv1_1" --scale 0.1 --shift -0.05
 
 
 import numpy as np
 import argparse
 import cv2
-
-# This disables the text written automatically by the Caffe C++ module
-# os.environ['GLOG_minloglevel'] = '2'
 import caffe
 
 
@@ -30,9 +27,9 @@ def get_arguments():
     Default False, set True for CPU mode')
     parser.add_argument('--terminal', action="store_true", help='\
     Set true for printing weights in terminal')
-    parser.add_argument('--scale_shift', type=float, default=0.0, help='\
+    parser.add_argument('--shift', type=float, default=0.0, help='\
     Substracts the data value by this before plot')
-    parser.add_argument('--scale_divide', type=float, default=1.0, help='\
+    parser.add_argument('--scale', type=float, default=1.0, help='\
     Divides the data value by this before plot')
     parser.add_argument('--autoscale', action="store_true", help='\
     Sets automatically the scale to better print the weights')
@@ -43,24 +40,8 @@ def get_arguments():
     
     return parser.parse_args()
 
-
-def build_network(args, caffemodel):
-    # GPU / CPU mode
-    if args.cpu:
-        print 'Set CPU mode'
-        # caffe.set_mode_cpu()
-    else:
-        print 'Set GPU mode'
-        caffe.set_mode_gpu()
     
-    # Creation of the network
-    net = caffe.Net(args.model,  # defines the structure of the model
-                    caffemodel,  # contains the trained weights
-                    caffe.TEST)  # use test mode (e.g., don't perform dropout)
-    return net
-
-    
-def vis_square(data, scale_shift, scale_divide, autoscale, label="Data"):
+def vis_square(data, shift=0, scale=1, autoscale=False):
     """Code by Caffe.
     Take an array of shape (n, height, width) or (n, height, width, 3)
     and visualize each (height, width) thing in a grid of size approx.
@@ -69,34 +50,35 @@ def vis_square(data, scale_shift, scale_divide, autoscale, label="Data"):
     # normalize data for display
     if data.max()-data.min() > 0:
             
-            if autoscale:
-                data = (data - data.min()) / (data.max() - data.min())
-            else:
-                data = (data-scale_shift)/scale_divide
-            
-            # force the number of filters to be square
-            n = int(np.ceil(np.sqrt(data.shape[0])))
-            padding = (((0, n ** 2 - data.shape[0]),
-                       (0, 1), (0, 1)) +  # add some space between filters
-                       ((0, 0),) * (data.ndim - 3))  # don't pad last dim
+        if autoscale:
+            shift = data.min()
+            scale = data.max() - data.min()
+            # data = (data - data.min()) / (data.max() - data.min())
+        # else:
+        data = (data - shift) / scale
 
-            # pad with ones (white)
-            data = np.pad(data, padding, mode='constant', constant_values=1)
+        # force the number of filters to be square
+        n = int(np.ceil(np.sqrt(data.shape[0])))
+        padding = (((0, n ** 2 - data.shape[0]),
+                   (0, 1), (0, 1)) +  # add some space between filters
+                   ((0, 0),) * (data.ndim - 3))  # don't pad last dim
 
-            # tile the filters into an image
-            # data = data.reshape((n, n) + data.shape[1:])
-            # axesOrder = (0, 2, 1, 3) + tuple(range(4, data.ndim + 1))
-            # data = data.transpose(axesOrder)
-            data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3) + tuple(range(4, data.ndim + 1)))
-            data = data.reshape((n * data.shape[1], n * data.shape[3]) +
-                                data.shape[4:])
-            
-            # Plot a 600x600 window of the weights
-            data_to_show = cv2.resize(data, (600, 600), 0, 0, 0,
-                                      cv2.INTER_NEAREST)
-            cv2.imshow(label, data_to_show)
-            
-            return data_to_show
+        # pad with ones (white)
+        data = np.pad(data, padding, mode='constant', constant_values=1)
+
+        # tile the filters into an image
+        # data = data.reshape((n, n) + data.shape[1:])
+        # axesOrder = (0, 2, 1, 3) + tuple(range(4, data.ndim + 1))
+        # data = data.transpose(axesOrder)
+        data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3) + tuple(range(4, data.ndim + 1)))
+        data = data.reshape((n * data.shape[1], n * data.shape[3]) +
+                            data.shape[4:])
+
+        # Plot a 600x600 window of the weights
+        data_to_show = cv2.resize(data, (600, 600), 0, 0, 0,
+                                  cv2.INTER_NEAREST)
+
+        return shift, scale, data_to_show
             
     else:
         print "Filter weights are all equal to ", data.max(), " !"
@@ -110,8 +92,19 @@ if __name__ == '__main__':
 
     # Run weight printing script
     print "Opening ", args.weights
+
+    # GPU / CPU mode
+    if args.cpu:
+        print 'Set CPU mode'
+        # caffe.set_mode_cpu()
+    else:
+        print 'Set GPU mode'
+        caffe.set_mode_gpu()
+    
     # Set the network according to the arguments
-    net = build_network(args, args.weights)
+    net = caffe.Net(args.model,  # defines the structure of the model
+                    args.weights,  # contains the trained weights
+                    caffe.TEST)  # use test mode (e.g., don't perform dropout)
 
     # Which layer to see ?
     layers = args.layers
@@ -142,10 +135,11 @@ if __name__ == '__main__':
             weights_printable = weights_printable[:, :, :, 0:3]
 
         # Plot all weights in a window
-        data_to_show = vis_square(weights_printable, args.scale_shift,
-                                  args.scale_divide, args.autoscale,
-                                  layer_to_see)
-
+        shift, scale, data_to_show = vis_square(weights_printable, args.shift,
+                                                args.scale, args.autoscale)
+        print ','.join([layer_to_see, str(shift), str(scale)])
+        cv2.imshow(layer_to_see, data_to_show)
+        
         if(args.save != ''):
             cv2.imwrite(args.save+'visualisation_'+str(args.weights)+'.png',
                         data_to_show)
