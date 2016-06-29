b2093755ce5cdbd4900326b777d7848f77eda259
Kim Samba <kim.95@hotmail.com>
2016-06-29 10:51:06 +0900

    added color argument convertimages.py

--- cocotool_script/convertimages.py
+++ cocotool_script/convertimages.py
@@ -4,6 +4,7 @@ import cv2
 import os
 import argparse
 import json
+import itertools
 
 def get_arguments():
     # Import arguments
@@ -14,6 +15,7 @@ def get_arguments():
     parser.add_argument('--data', type=str, help='\
     path of images folder where jpg files can be found')
     parser.add_argument('--ann', type=str, help='annotation file path', default='')
+    parser.add_argument('--color', type=bool, help='input color images', default=False)
     return parser.parse_args()
 
 def getPriority(id):
@@ -21,11 +23,11 @@ def getPriority(id):
 	# swiss 100 
 	#if id == 3 or id == 4 or id == 6 or id == 7:
 	#okutama
-	if id == 5 or id == 4 or id == 3:
+	if id == 5 or id == 4 or id == 3 or id == 13 or id == 14:
 		return 1
 	return 0
 
-def drawSegmentation(image, anns, img):
+def drawSegmentation(image, anns, color):
         """
         draws segmentation on input image
         :param anns (array of object): annotations to display
@@ -35,11 +37,14 @@ def drawSegmentation(image, anns, img):
             return False
         if 'segmentation' in anns[0]:
 	    # sort annotations from biggest to smallest to avoid occlusions
-	    colours = cv2.imread('/home/shared/data/datasets/VOCdevkit/VOC2012/colours/pascal_voc_21_colours.png').astype(np.uint8)
+	    colours = cv2.imread('/home/shared/data/datasets/Okutama/colours/okutama_colours.png').astype(np.uint8)
 	    anns.sort(key=lambda x: (getPriority(x['category_id']),x['area']), reverse=True)
             for ann in anns:
-		#c = [int(colours[0][ann['category_id']][0]), int(colours[0][ann['category_id']][1]), int(colours[0][ann['category_id']][2])] 
-		c = [ann['category_id'], ann['category_id'], ann['category_id']]
+		#print human readable colors
+		if color:
+			c = [int(colours[0][ann['category_id']][0]), int(colours[0][ann['category_id']][1]), int(colours[0][ann['category_id']][2])] 
+		else:
+			c = [ann['category_id'], ann['category_id'], ann['category_id']]
 		if type(ann['segmentation']) == list:
 		    poly = np.array(ann['segmentation']).reshape((len(ann['segmentation'])/2, 2))
 		    pts = np.array(poly, np.int32)
@@ -48,9 +53,6 @@ def drawSegmentation(image, anns, img):
 		    cv2.fillPoly(image, [pts], c)
 	return True
 		    
-
-		 
-
 
 if __name__ == '__main__':
 
@@ -81,24 +83,29 @@ if __name__ == '__main__':
 	for img in imgs:
 		#open image
 		#image = cv2.imread('%s/images/%s/%s'%(dataDir,dataType,img['file_name']),-1)
-		image = cv2.imread('%s/%s'%(dataDir,img['file_name']),-1)
+		for subdir, dirs, files in os.walk(dataDir):
+			image = cv2.imread('%s/%s'%(subdir,img['file_name']),-1)
+			if image != None:
+				break
 		
 		if image==None:
 			continue		
-			
+		
 		#if there is no color channels, resize the matrix
 		
 		if len(image.shape) is 2:
 		        image = np.resize(image, (image.shape[0],image.shape[1],3))
 		
-		#set all pixel to background class
+		#set all pixels to background class
 	 	image[:] = 0
 		# load instance annotations
-		annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds)		
-		anns = coco.loadAnns(annIds)
-
+		#annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds)	
+		lists = [coco.imgToAnns[img['id']] for imgId in imgIds if imgId in coco.imgToAnns]
+                anns = list(itertools.chain.from_iterable(lists))
+		#anns = coco.loadAnns(annIds)
+		
 		#apply segmentations on image	
-		suitable = drawSegmentation(image, anns, img)
+		suitable = drawSegmentation(image, anns, args.color)
 		
 		if not suitable:
 			continue
@@ -106,15 +113,16 @@ if __name__ == '__main__':
 		print img['file_name']
 
 		#display image
+
 		#cv2.imshow('image',image)
 		#cv2.waitKey(0)
 		#cv2.destroyAllWindows()
 
-		#save files
+		#save image
+
 		if not os.path.isdir(dst):
 			os.makedirs(dst)
 		path=dst+'/'+str(img['file_name'])[:-4]+'.png'
-
 		cv2.imwrite(path, image)
 
 
