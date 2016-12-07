5e136a96828d00a8df2b57eb3dab3b5d68878fb3
Miquel Marti <miquelmr@kth.se>
2016-12-07 20:42:24 +0900

    Fix create logit

--- caffeTools/segment.py
+++ caffeTools/segment.py
@@ -31,7 +31,7 @@ class LogitCollection(object):
     def __init__(self, logit_col_buf):
         self.directory = logit_col_buf.folder
         self.weight = logit_col_buf.weighting
-        
+
     def getImageLogits(self, image_basename):
         image_basename = os.path.splitext(image_basename)[0]
         logit_filename = self.directory + image_basename + '.npy'
@@ -68,7 +68,7 @@ def get_arguments():
     # Optional arguments
     parser.add_argument('--cpu', action="store_true", help='\
     Default false, set it for CPU mode')
-    parser.add_argument('--gpu_device', type=int, default=None, help='\
+    parser.add_argument('--gpu_device', type=int, default=0, help='\
     Default 0, set the GPU device to use')
     uiGroup = parser.add_mutually_exclusive_group()
     uiGroup.add_argument('--key', action="store_true", help='\
@@ -99,23 +99,23 @@ def build_network(deploy, weights):
     net = caffe.Net(str(deploy),      # defines the structure of the model
                     str(weights),    # contains the trained weights
                     caffe.TEST)  # use test mode (e.g., don't perform dropout)
-    
+
     return net
 
 
 def colourSegment(labels, label_colours):
     # Resize it for 3 channels, now (3, 360, 480)
     segmentation_ind_3ch = np.resize(labels, (3,) + labels.shape)
-    
+
     # Converts it to format H x W x C (was C x H x W)
     segmentation_ind_3ch = segmentation_ind_3ch.transpose(1, 2, 0)
     segmentation_ind_3ch = segmentation_ind_3ch.astype(np.uint8)
-    
+
     # Colour in a new image with the same dimensions
     _output = np.zeros(segmentation_ind_3ch.shape, dtype=np.uint8)
     cv2.LUT(segmentation_ind_3ch, label_colours, _output)
     return _output
-    
+
 
 def softmax(x):
     '''Softmax function, transforming logits into probabilities'''
@@ -123,44 +123,44 @@ def softmax(x):
     out = out / np.sum(out, axis=0)
     return out
 
-    
+
 def combineEnsemble(net_outputs, method, weighting):
     #This method will combine the output of models forming an Ensemble
     output = 0
-    
+
     # If there is only one model, we skip this step
     if np.asarray(net_outputs).shape[0] == 1:
         return np.squeeze(net_outputs)
-    
+
     if method == ensemble_pb2.VOTING:  # Majority voting
             # Calculates the label (by looking at the maximum class score)
             net_outputs = np.squeeze(np.asarray(net_outputs).argmax(axis=1))
-            
+
             # Looks for the most common label for each pixel
             output = np.squeeze(stats.mode(net_outputs, axis=0)[0]).astype(int)
-    
+
     if method == ensemble_pb2.LOGITARI:  # Logit arithmetic averaging
-            output = np.zeros(net_outputs[0].shape)     
+            output = np.zeros(net_outputs[0].shape)
 
             sum_weightings = 0
             for net_output, current_weighting in zip(net_outputs, weighting):
                 output = output + net_output * current_weighting
                 sum_weightings = sum_weightings + current_weighting
-            
+
             # Make it a mean instead of a sum of logits
             output = output/sum_weightings
-            
+
     if method == ensemble_pb2.LOGITGEO:  # Logit geometric averaging (NOT WORKING, BECAUSE LOGITS CAN BE NEGATIVE)
-            output = np.ones(net_outputs[0].shape)     
-            
+            output = np.ones(net_outputs[0].shape)
+
             for net_output in net_outputs:
                 output = np.multiply(output, net_output)
-            
+
             # Make it a mean
             output = np.power(output, float(1)/len(net_outputs))
-    
+
     if method == ensemble_pb2.PROBAARI:  # Probability arithmetic averaging
-        output = np.zeros(net_outputs[0].shape)     
+        output = np.zeros(net_outputs[0].shape)
 
         sum_weightings = 0
         for net_output, current_weighting in zip(net_outputs, weighting):
@@ -169,9 +169,9 @@ def combineEnsemble(net_outputs, method, weighting):
 
         # Make it a mean instead of a sum of probabilities
         output = output/sum_weightings
-    
+
     if method == ensemble_pb2.PROBAGEO:  # Probability geometric averaging
-        output = np.ones(net_outputs[0].shape)     
+        output = np.ones(net_outputs[0].shape)
 
         for net_output in net_outputs:
             output = np.multiply(output, softmax(net_output))
@@ -195,7 +195,7 @@ def cropAndSegment(input_image, num_sections, model, mean):
     '''
     input_array = np.asarray(input_image)
     # Set output segmentation shape
-    
+
     # model_logits = np.zeros((model.getNumClasses(), input_array.shape[0],
     #                          input_array.shape[1]))
     model_logits = np.zeros((model.getNumClasses(),) + input_array.shape[:2])
@@ -212,7 +212,7 @@ def cropAndSegment(input_image, num_sections, model, mean):
             # Why do we take [0] here???
             logits = model.segmentImage(cropped_image, mean)[0]
             # print 'logit shape:', logits.shape
-            
+
             # Get segmentation with cropped image
             model_logits[:, h_start:h_end, w_start:w_end] = logits
     return model_logits
@@ -225,7 +225,7 @@ def computeEnsembleLogits(input_image, input_shape, models, logit_cols, crop):
     # TODO maybe don't need input_shape - do the commented instead
     # crop_shape = (np.asarray(np.asarray(input_image).shape) / crop)
     # new_shape = np.array([1, 3, crop_shape[0], crop_shape[1]])
-    
+
     new_shape = None
     if config.input.resize:
         new_shape = input_shape
@@ -241,7 +241,7 @@ def computeEnsembleLogits(input_image, input_shape, models, logit_cols, crop):
             model_logits = cropAndSegment(input_image, crop, model, mean)
         else:
             model_logits = model.segmentImage(input_image, mean, new_shape)
-        logits.append(np.squeeze(model_logits[0]))
+        logits.append(np.squeeze(model_logits))
 
     # Get the time after the network process
     runTime = time.time() - start
@@ -274,7 +274,7 @@ def displayOutput(label_colours, input_image, guessed_labels, true_labels,
                int(resize*input_image.shape[0]))
     cv2.imshow("Input", cv2.resize(input_image, newSize))
     cv2.imshow("Output", cv2.resize(guessed_image, newSize))
-    
+
     if true_labels is not None:
         # display the ground truth as well
         gt_image = np.array(true_labels)
@@ -314,12 +314,12 @@ def writeVideo(images, segmentation, input_image, guessed_labels, label_colours,
                int(resize*input_image.shape[0]))
         images.write(cv2.resize(input_image, newSize))
         segmentation.write(cv2.resize(guessed_image, newSize))
-        
+
         return (images, segmentation)
 
 if __name__ == '__main__':
     args = get_arguments()
-    
+
     # GPU / CPU mode
     if args.cpu:
         print 'Set CPU mode'
@@ -329,15 +329,15 @@ if __name__ == '__main__':
         if args.gpu_device is not None:
                 caffe.set_device(args.gpu_device)
         caffe.set_mode_gpu()
-    
+
     # Read the ensemble.prototxt
     config = ensemble_pb2.Ensemble()
     protoUtils.readFromPrototxt(config, args.ensemble_file)
-    
+
     if not config.IsInitialized():
         raise ValueError('\
         Prototxt not complete (not all required fields are present)')
-    
+
     # Load all networks and model output folders specified in .proto
     models = []
     for model_buf in config.model:
@@ -345,15 +345,15 @@ if __name__ == '__main__':
     logit_cols = []
     for logitFolderBuf in config.logitFolder:
         logit_cols.append(LogitCollection(logitFolderBuf))
-    
+
     # get the various dimensions of the network
     input_shape = default_input_shape
     numb_cla = default_numb_cla
     if len(models) > 0:
         input_shape = models[0].getInputShape()
         numb_cla = models[0].getNumClasses()
-    
-        
+
+
     # Create the appropriate image iterator
     imageIterator = None
     if config.input.type == ensemble_pb2.Input.VIDEO:
@@ -367,16 +367,16 @@ if __name__ == '__main__':
                                                    pairs=True)
     else:
         raise ValueError("No data provided in the prototxt!")
-        
+
     # If an output folder is provided, create a list.txt file for summarising
     # which numpy matrices of which images have been created
     if config.outputFolder != "":
         summaryFile = open(config.outputFolder+"list.txt", 'w')
-    
+
     mean = np.array([config.input.mean.r,
                      config.input.mean.g,
                      config.input.mean.b])
-                     
+
     # process each image, one-by-one
     n_im = 1  # Image counter
     times = []  # Variable for test times
@@ -398,7 +398,7 @@ if __name__ == '__main__':
             input_image = input_image.crop((a, b, a+dw, b+dh))
             real_label = real_label.crop((a, b, a+dw, b+dh))
             print a,b,a+dw,b+dh
-    
+
         #If we want to resize all inputs to the same size
         if config.input.resize:
             input_image = input_image.resize((input_shape[3], input_shape[2]),
@@ -420,17 +420,15 @@ if __name__ == '__main__':
             logits = logits[0][0]
         elif len(logits.shape) == 1:
             logits = logits[0]
-
-
         # If we want to save network outputs to folder
         if config.outputFolder != "":
             np.save(config.outputFolder + image_name + ".npy", logits)
             #Save the stored outputs in a summary list file
             summaryFile.write(image_path + " " + config.outputFolder +
                               image_name + ".npy\n")
-        
 
-            
+
+
         # label each pixel as the class with the biggest logit
         if len(logits.shape) == 3:
             guessed_labels = logits.argmax(axis=0)
@@ -441,7 +439,7 @@ if __name__ == '__main__':
         #If ground-truth is provided, compute the metrics
         if real_label is not None:
             real_label = real_label.resize(input_image.size, Image.NEAREST)
-            
+
             # If pascal VOC, reshape the label to HxWx1s
             np_real_label = np.array(real_label)
             if len(np_real_label.shape) == 3:
@@ -458,13 +456,13 @@ if __name__ == '__main__':
                 acc = score.computeSegmentationScores(hist).overallAcc
                 print "Image", n_im, "(", image_name, ") accuracy:", acc
 
-                     
+
         # Switch to RGB (PIL Image read files as BGR)
         if len(np.array(input_image).shape) is 3:
             input_image = np.array(cv2.cvtColor(np.array(input_image),
                                                 cv2.COLOR_BGR2RGB))
-        
-        #If we want to record a video  
+
+        #If we want to record a video
         if args.record != '':
                 shape = (int(input_image.shape[1]*args.view_resize), int(input_image.shape[0]*args.view_resize))
                 if n_im == 1: #If it's the first image, we initialise video
@@ -472,8 +470,8 @@ if __name__ == '__main__':
                         images, segmentation = initialiseVideo(shape)
                 print "Writing frame", n_im
                 images, segmentation = writeVideo(images, segmentation, input_image, guessed_labels, label_colours, args.view_resize)
-        
-                
+
+
         # Display input and output
         if display:
             if args.show_prob is not None:
@@ -482,7 +480,7 @@ if __name__ == '__main__':
                                 real_label, args.view_resize, args.key)
             if key % 256 == 27:  # exit on ESC - keycode is platform-dependent
                 break
-        
+
         n_im += 1
 
     # Exit properly
@@ -492,14 +490,14 @@ if __name__ == '__main__':
         #labels.release()
     elif not args.hide:
         cv2.destroyAllWindows()
-        
+
     # If we opened an output file, close it
     if config.outputFolder != "":
         summaryFile.close()
-    
+
     avgImageTime = sum(times) / float(len(times))
     print "Average time elapsed when processing one image :\t", avgImageTime
-    
+
     if config.input.type == ensemble_pb2.Input.LABELS:
         # Display the metrics
         scores = score.computeSegmentationScores(totalHist)
